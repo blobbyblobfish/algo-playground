398b40ebe00ec59b3b68cff978410bb07bea97f4
blobbyblobfish <aywang6@gmail.com>
2020-08-29 22:37:07 -0400

    Roman to integer

---
+++ strings/RomanToInt1.js
@@ -0,0 +1,56 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// Solution 1: 
+    // Create a variable to hold the total.
+    // Parse the string.
+    // For each character of the string, add the corresponding value to total...
+        // EXCEPT:
+        // For I followed by V or X
+        // For X followed by L or C
+        // For C followed by D or M
+        // Add the corresponding value to total and skip the next character.
+
+const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+}
+
+var romanToInt = function(s) {
+    let total = 0
+    
+    for (i = 0; i < s.length; i++) {
+        
+        if (s[i] === "V" && s[i-1] === "I" || s[i] === "X" && s[i-1] === "I") {
+            total = total + values[s[i]] - (2 * values[s[i-1]]) 
+            continue
+        }
+        
+        if (s[i] === "L" && s[i-1] === "X" || s[i] === "C" && s[i-1] === "X") {
+            total = total + values[s[i]] - (2 * values[s[i-1]])
+            continue
+        }
+        
+        if (s[i] === "D" && s[i-1] === "C" || s[i] === "M" && s[i-1] === "C") {
+            total = total + values[s[i]] - (2 * values[s[i-1]]) 
+            continue
+        }
+        
+        else {
+            total = total + values[s[i]]
+        }
+    }
+    
+    return total
+}
+
+// Notes:
+    // Odd behavior: "MCMXCIV" function was allowing BOTH if and else statements to run.
+        // For second C and V but not second M.
